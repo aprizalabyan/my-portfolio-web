70dfd91c1ec371ea3b28b04638524feb77049afb
aprizalabyan <aprizalabyan@gmail.com>
2023-05-13 19:51:04 +0800

    Add portfolio

--- src/components/CompPortfolio.js
+++ src/components/CompPortfolio.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Carousel from 'react-multi-carousel'
 import port1 from './../assets/port-1.png'
 import port2 from './../assets/port-2.png'
+import port3 from './../assets/port-3.png'
 
 const CompPortfolio = () => {
     const responsive = {
@@ -44,6 +45,16 @@ const CompPortfolio = () => {
                         <h5 className="card-title">Web Design</h5>
                         <h6 className="card-title">Admin Dashboard</h6>
                         <a href="https://aprizalabyan.github.io/e-gym-dashboard/" target="_blank" rel="noreferrer">
+                            <p className="card-text fst-italic">Preview <i className="bi bi-arrow-right"></i></p>
+                        </a>
+                    </div>
+                </div>
+                <div className="card col-8">
+                    <img src={port3} className="card-img-top" alt="img"></img>
+                    <div className="card-body">
+                        <h5 className="card-title">Web App</h5>
+                        <h6 className="card-title">Spreadsheet API</h6>
+                        <a href="https://aprizalabyan.github.io/contact-list/" target="_blank" rel="noreferrer">
                             <p className="card-text fst-italic">Preview <i className="bi bi-arrow-right"></i></p>
                         </a>
                     </div>
